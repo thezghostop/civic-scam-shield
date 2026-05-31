66fa024362a8706220f2a5905545dcdcff1ce0b8
thezghostop <chaitanyachalithoff@gmail.com>
2026-05-31 12:35:36 +0530

    fix: allow all CORS origins for Vercel deployment

--- Chaitanya_Backend/main.py
+++ Chaitanya_Backend/main.py
@@ -10,10 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://*.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
